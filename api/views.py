4a6a9e16a861f7996b21e3b5138b89875cb14c0c
rifat977 <itscrifat5147@gmail.com>
2022-10-02 08:15:53 +0600

    initial

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from yaml import serialize
 from .models import *
 import api.serializers as api_ser
 from rest_framework import status
